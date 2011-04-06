90e0b05f7971bb987bc8ceca7a8b4a7b747edf3d
namjk24@gmail.com <namjk24@gmail.com@e3ab8832-cd79-4ee9-3d71-893b5bf6c76a>
2011-04-06 00:26:03 +0000

    Socket 네트워크 샘플 추가

---
+++ trunk/kamoru/src/kamoru/test/serialization/socket/JabberServer.java
@@ -0,0 +1,41 @@
+package kamoru.test.serialization.socket;
+
+import java.io.*;
+import java.net.*;
+
+public class JabberServer {
+
+	static final int port = 8888;
+
+	public static void main(String[] args) {
+		try {
+			MyObject o = new MyObject();
+			ServerSocket s = new ServerSocket(port);
+			System.out.println("[SERVER] Server Start\n\t" + s + "\n\tListens for a connection to be made ");
+			
+			Socket socket = s.accept();
+			System.out.println("[SERVER] Connection accepted \n\t" + socket);
+
+			ObjectOutputStream toClient = new ObjectOutputStream(
+					socket.getOutputStream());
+			DataInputStream fromClient = new DataInputStream(
+					socket.getInputStream());
+
+			while (o.count < 6) {
+				o.setName();
+				toClient.reset();
+				toClient.writeObject(o);
+				System.out.println("[SERVER] writing " + o);
+
+				System.out.println("[SERVER] trying to received acknowledgement...");
+				System.out.println("[SERVER] acknowledgement:" + fromClient.readInt());
+				System.out.println("[SERVER] succeeded");
+			}
+			System.out.println("[SERVER] closing...");
+			toClient.close();
+			socket.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ trunk/kamoru/src/kamoru/test/serialization/socket/MyObject.java
@@ -0,0 +1,22 @@
+package kamoru.test.serialization.socket;
+
+import java.io.*;
+
+public class MyObject implements Serializable {
+
+	String name;
+	int count;
+
+	MyObject() {
+		setName();
+	}
+
+	public void setName() {
+		count++;
+		name = "MyObject" + count;
+	}
+
+	public String toString() {
+		return name;
+	}
+}
