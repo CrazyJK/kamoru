bd72fa073436bc0ac36cdb5e6f793d47a45842c7
namjk24@gmail.com <namjk24@gmail.com@e3ab8832-cd79-4ee9-3d71-893b5bf6c76a>
2013-02-27 13:27:30 +0000

    Spring video - home work 2/27

---
+++ trunk/kamoruSpring/src/main/java/kamoru/app/spring/TestController.java
@@ -0,0 +1,62 @@
+package kamoru.app.spring;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Random;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.IOFileFilter;
+import org.apache.tika.Tika;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value="/test/image", method=RequestMethod.GET)
+	public HttpEntity<byte[]> image() throws IOException {
+		byte[] imageBytes = imageBytes();
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.IMAGE_JPEG);
+		headers.setContentLength(imageBytes.length);
+		headers.setCacheControl("max-age=" + 86400);
+		headers.setDate(new Date().getTime() + 86400*1000l);
+		headers.setExpires(new Date().getTime() + 86400*1000l);
+		return new HttpEntity<byte[]>(imageBytes, headers);
+	}
+
+	@RequestMapping(value="/test/image2", method=RequestMethod.GET)
+	public void image2(HttpServletResponse response) throws IOException {
+		byte[] imageBytes = imageBytes();
+		
+		Tika tika = new Tika();
+//	    String mimeType = tika.detect(imageFile);
+//		response.setContentType(mimeType);
+		response.setDateHeader("Expires", new Date().getTime() + 86400*1000l);
+		response.setHeader("Cache-Control", "max-age=" + 86400);
+		response.getOutputStream().write(imageBytes);
+		response.getOutputStream().flush();
+		response.getOutputStream().close();
+	}
+
+	private byte[] imageBytes() throws IOException {
+		File directory = new File("E:\\Girls");
+		Collection<File> found = FileUtils.listFiles(directory, new String[]{"jpg"}, true);
+		File[] files = found.toArray(new File[0]);
+		int random = new Random().nextInt(files.length);
+//		random = 0;
+		File imageFile = files[random];
+		return FileUtils.readFileToByteArray(imageFile);
+	}
+
+}
